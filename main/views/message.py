6a2997b464d7d3a174bf50a1e58d067cedc8b166
jerry <khanh96le@gmail.com>
2018-11-07 19:32:02 +0700

    fix(*): handle exception

--- main/views/message.py
+++ main/views/message.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, jsonify
-from hl7tojson import parser
 from flask import request
+from hl7tojson import parser
 
 blueprint = Blueprint('article', __name__)
 
 
 @blueprint.route('/messages', methods=['POST'])
 def get_message():
-    message = request.get_json()
-    return jsonify(parser.parse(message))
+    data = request.get_json()
+    message = data['message']
+    message = message.replace('\n', '\r').encode('utf-8')
+    try:
+        parsed_data = parser.parse(message)
+    except Exception as e:
+        return jsonify({
+            'error': e.message
+        }), 400
+    return jsonify(parsed_data)
